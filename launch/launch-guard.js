95578bb999c9c7066b1b7428e8dbf9aebdfd3293
Roc <>
2023-04-19 16:45:53 +0800

    .

--- launch/launch-guard.js
+++ launch/launch-guard.js
@@ -1,5 +1,5 @@
-const version = "1.0.0.40";
-const proxy_name = "Weibo Ad Block";
+const version = "1.0.1";
+const proxy_name = "Weibo Launch Ad Block";
 console.log(`${proxy_name}: ${version}`);
 
 const path1 = "/v1/ad/preload";
@@ -8,11 +8,16 @@ const path2 = "/wbapplua/wbpullad.lua";
 const url = $request.url;
 let body = $response.body;
 
+console.log(`url: ${url}`);
+
 if (url.indexOf(path1) != -1) {
+
+  console.log(`匹配到 ${path1}`);
+
   let obj = JSON.parse(body);
   if (obj.realtime_video_stall_time) obj.realtime_video_stall_time = 0;
   if (obj.ads) obj.ads = [];
-  if (obj.last_ad_show_interval) obj.last_ad_show_interval = 0;
+  if (obj.last_ad_show_interval) obj.last_ad_show_interval = 60 * 60 * 24 * 365;
   if (obj.realtime_api_timeout) obj.realtime_api_timeout = 0;
   if (obj.realtime_stop_conf) obj.realtime_stop_conf = {
     "api_timeout": 0,
@@ -25,6 +30,7 @@ if (url.indexOf(path1) != -1) {
 }
 
 if (url.indexOf(path2) != -1) {
+  console.log(`匹配到 ${path2}`);
   let obj = JSON.parse(body);
   if (obj.cached_ad && obj.cached_ad.ads) obj.cached_ad.ads = [];
   if (obj.cached_ad && obj.cached_ad.delete_days) obj.delete_days = 0;
