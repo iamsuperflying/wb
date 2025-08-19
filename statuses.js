88f01623cb4fc81ad955545357048dcd560efb77
Roc <no@thank.you>
2025-08-19 15:57:09 +0800

    refactor(statuses.js): 修复微博详情页状态对象结构
    更新了微博详情页的状态对象结构，移除了通用结构中的广告项，确保状态对象的返回结构更加简洁，提升了代码的可读性和维护性。

--- statuses.js
+++ statuses.js
@@ -166,12 +166,13 @@ function rwDetailAd(data) {
     
     // 移除通用结构中的广告
     status.common_struct = [];
-    data.detailInfo.status = [];
+    data.detailInfo.status.common_struct = [];
     // delete status.common_struct;
 
     // 移除小店
     status.tag_struct = [];
     delete status.tag_struct;
+
   
   }
   
